efec074d802c03b23cdd0605687d36df84e1d4c1
Toshiaki Takada <toshiaki.takada@gmail.com>
2014-05-04 06:03:07 +0000

    Modify rails setup to handle add index with specified keys.

--- www/setup.rb
+++ www/setup.rb
@@ -29,12 +29,6 @@ require 'json'
 rails_project = "zebra"
 db_dir = "../db"
 
-# Create rails project
-# system("rails new #{rails_project}")
-Dir.chdir(rails_project)
-
-dir = "../" + db_dir
-
 def rails_data_type(obj)
   type = obj["type"]
   if type == "enum"
@@ -48,33 +42,99 @@ def rails_data_type(obj)
   type
 end
 
+def rails_table_name(table_name)
+  table_name.split("-").map(&:capitalize).join("")
+end
+
+def rails_add_index(table_name, keys)
+  name = table_name.gsub(/\-/, "_") + "s"
+
+  migration = "add_index_to_" + name
+
+  rails_cmd = "rails generate migration " + migration
+  system(rails_cmd)
+
+  Dir.chdir("db/migrate")
+  m = Dir.entries(".").select {|f| f =~ /#{migration}\.rb$/}
+
+  if m.size == 1
+    migration_file = m[0]
+
+    lines = Array.new
+    File.open(migration_file, "r") do |f|
+      while line = f.gets
+        lines << line
+      end
+    end
+
+    keys_str = keys.map {|k| '"' + k.gsub(/\-/, "_") + '"'}.join(", ")
+    File.open(migration_file, "w") do |f|
+      f.puts lines[0]
+      f.puts lines[1]
+      f.puts '    add_index "' + name +
+        '", [' + keys_str + '], :unique => true'
+      f.puts lines[2]
+      f.puts lines[3]
+    end
+
+  else
+    puts "Couldn't find migration file: " + migration
+  end
+
+  Dir.chdir("../..")
+end
+
 def rails_scaffolding(json)
   json["table"].each do |table_name, table_def|
     children = table_def["children"]
 
     fields = Array.new
+
+    # Association
+    if table_def["parent"] != nil
+      fields << table_def["parent"].gsub(/\-/, "_") + "_id:integer"
+    end
+
+    # Table keys
     table_def["keys"].each do |k, obj|
       key = k.gsub(/\-/, "_");
       fields << key + ":" + rails_data_type(obj)
     end
 
+    # Other fields
     table_def["attributes"].each do |k, obj|
       key = k.gsub(/\-/, "_");
       fields << key + ":" + rails_data_type(obj)
     end
 
-    str = "rails generate scaffold " + fields.join(" ")
-    puts str
+    # Scaffolding
+    rails_cmd = "rails generate scaffold " +
+      rails_table_name(table_name) + " " +
+      fields.join(" ")
+    puts rails_cmd
+    system(rails_cmd)
 
-#    system("rails generate scaffold")
+    # Generate index
+    rails_add_index(table_name, table_def["keys"].keys)
   end
 end
 
-# Iterate table.json files
-Dir.entries(dir).each do |f|
-  /\.table\.json$/.match(f) do
-    str = File.read(dir + "/" + f)
-    json = JSON.parse(str)
-    rails_scaffolding(json)
+def main(rails_project, db_dir)
+  # Create rails project
+  # system("rails new #{rails_project}")
+  Dir.chdir(rails_project)
+
+  dir = "../" + db_dir
+
+  # Iterate table.json files
+  Dir.entries(dir).each do |f|
+    /\.table\.json$/.match(f) do
+      str = File.read(dir + "/" + f)
+      json = JSON.parse(str)
+
+      rails_scaffolding(json)
+    end
   end
 end
+
+main(rails_project, db_dir)
