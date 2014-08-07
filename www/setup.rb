77bcc7c3a0eaa125755a6bf6f78a3f29608c0f2b
Toshiaki Takada <toshiaki.takada@gmail.com>
2014-08-07 22:04:50 +0000

    Now, scaffolding is based on timestamp of *.table.json. That will trigger scaffolding, and migration down if it is updated. Also add_index migration is now ERB.

--- www/setup.rb
+++ www/setup.rb
@@ -29,49 +29,7 @@ require 'getoptlong'
 require 'json'
 require 'erb'
 
-# Mandatory arguments
-#  [0] rails project
-#  [1] table.json directory
-#
-if ARGV[0] != nil
-  $rails_project = ARGV[0]
-else
-  $rails_project = "railsapp"
-end  
-
-if ARGV[1] != nil
-  db_dir = ARGV[1]
-else
-  db_dir = "../db"
-end
-
-def show_help
-  puts "Usage: this-script <rails-project> <table-def-dir> [options...]"
-  exit
-end
-
-def set_options_all(options)
-  options[:scaffold] = true
-  options[:migration] = true
-  options[:model] = true
-  options[:controller] = true
-  options[:view] = true
-  options[:helper] = true
-  options[:routes] = true
-  options[:mime] = true
-end
-
-def unset_options_all(options)
-  options[:scaffold] = false
-  options[:migration] = false
-  options[:model] = false
-  options[:controller] = false
-  options[:view] = false
-  options[:helper] = false
-  options[:routes] = false
-  options[:mime] = false
-end
-
+
 # Utilities
 def keyword(k)
   k.gsub(/\-/, "_")
@@ -89,6 +47,54 @@ def keyword_plural(k)
   keyword(k) + "s"
 end
 
+
+# Execute external commands.
+def rails_scaffold(table_name, db_fields, force)
+  name = keyword(table_name)
+
+  cmd = "rails generate scaffold " + name + " " +
+    db_fields.map{|a| a[0] + ":" + a[1] }.join(" ")
+  cmd += " --force" if force == true
+
+  puts "=> Scaffolding " + name + " ..."
+  puts "# " + cmd
+
+  system cmd
+end
+
+def rails_rake_db_migrate_down(table_name)
+  cmd = "rake db:migrate:down VERSION=" + $tables[:mig_version][table_name]
+
+  puts "=> DB migrate down " + keyword(table_name) + " ..."
+  puts "# " + cmd
+
+  system cmd
+end
+
+def rails_rake_db_migrate
+  cmd = "rake db:migrate"
+
+  puts "=> Run DB migration ..."
+  puts "# " + cmd
+
+  system cmd
+end
+
+def rails_migration(table_name)
+  name_p = keyword_plural(table_name)
+  migration = "add_index_to_" + name_p
+
+  cmd = "rails generate migration " + migration + " --force"
+
+  puts "=> Generate migration to add index to " + name_p + "... "
+  puts "# " + cmd
+
+  system cmd
+
+  [name_p, migration]
+end
+
+
 # Rails related
 def rails_data_type(obj)
   type = obj["type"]
@@ -104,14 +110,10 @@ def rails_data_type(obj)
 end
 
 def rails_db_add_index(table_name, table_keys)
-  puts "=> Add index to '" + keyword(table_name) + "' migrations..."
-
+  # Generate migration as a place holder.
+  name_p, migration = rails_migration(table_name)
   index_keys = table_keys.map{|t| t[0]}
-  name = keyword_plural(table_name)
-  migration = "add_index_to_" + name
-
-  rails_cmd = "rails generate migration " + migration
-  system(rails_cmd)
+  template = File.read("../add_index.erb")
 
   Dir.chdir("db/migrate")
   m = Dir.entries(".").select {|f| f =~ /#{migration}\.rb$/}
@@ -119,32 +121,123 @@ def rails_db_add_index(table_name, table_keys)
   if m.size == 1
     migration_file = m[0]
 
-    lines = Array.new
-    File.open(migration_file, "r") do |f|
-      while line = f.gets
-        next if line =~ /add_index/
-
-        lines << line
-      end
-    end
-
-    # All index keys
-    keys_str = index_keys.map {|k| '"' + keyword(k) + '"'}.join(", ")
-    index_name = name + "_index"
     File.open(migration_file, "w") do |f|
-      f.puts lines[0]
-      f.puts lines[1]
-      f.puts '    add_index "' + name +
-        '", [' + keys_str + "], :unique => true, :name => '#{index_name}'"
-      f.puts lines[2]
-      f.puts lines[3]
-    end
+      @class_name = keyword_camel(table_name)
+      @index_name = name_p
 
+      # All index keys
+      @keys_str = index_keys.map {|k| '"' + keyword(k) + '"'}.join(", ")
+
+      # Generate migration
+      renderer = ERB.new(template, nil, '<>')
+      f.puts renderer.result()
+    end
   else
     puts "Couldn't find migration file: " + migration
   end
 
   Dir.chdir("../..")
+end
+
+def rails_get_migration_create(table_name)
+  name = keyword_plural(table_name)
+  mig = "create_" + name
+  mig_create = nil
+  mtime = nil
+  version = nil
+
+  if File.exists?("db/migrate")
+    Dir.chdir("db/migrate")
+    files = Dir.entries(".").select {|f| f =~ /#{mig}\.rb$/}
+    
+    if files.size == 1
+      mig_create = files[0]
+      mtime = File.mtime(mig_create)
+      m = /^(\d+)_#{mig}\.rb$/.match(mig_create)
+      if m != nil
+        version = m[1]
+      end
+    end
+
+    Dir.chdir("../..")
+  end
+
+  [mig_create, mtime, version]
+end
+
+def rails_list_tables(tables, type)
+  tables[type].each do |t|
+    if tables[:mig_create][t].nil?
+      tables[:flag][t] = "N"
+    elsif tables[:mig_mtime][t] < tables[:mtime][t]
+      tables[:flag][t] = "U"
+    else
+      tables[:flag][t] = " "
+    end
+
+    version = "N/A"
+    version = tables[:mig_version][t]
+    printf("%s %-32s %s\n", tables[:flag][t], keyword(t), version)
+  end
+end
+
+def rails_load_tables(dir)
+  tables = Hash.new
+  tables[:parents] = Array.new
+  tables[:dependents] = Array.new
+  tables[:associations] = Array.new
+  tables[:json] = Hash.new
+  tables[:mtime] = Hash.new
+  tables[:mig_create] = Hash.new
+  tables[:mig_mtime] = Hash.new
+  tables[:mig_version] = Hash.new
+  tables[:flag] = Hash.new
+
+  m = Dir.entries(dir).select {|f| f =~ /\.table\.json$/}
+  m.each do |f|
+    file = dir + "/" + f
+
+    str = File.read(file)
+    mtime = File.mtime(file)
+    json = JSON.parse(str)
+
+    if json["table"] != nil
+      json["table"].each do |table_name, obj|
+        if obj["type"] != nil
+          if obj["type"] == "parent"
+            tables[:parents] << table_name
+          elsif obj["type"] == "association"
+            tables[:associations] << table_name
+          elsif
+            tables[:dependents] << table_name
+          end
+        end
+
+        tables[:json][table_name] = obj
+        tables[:mtime][table_name] = mtime
+
+        mig_create, mig_mtime, mig_version =
+          rails_get_migration_create(table_name)
+        tables[:mig_create][table_name] = mig_create
+        tables[:mig_mtime][table_name] = mig_mtime
+        tables[:mig_version][table_name] = mig_version
+      end
+    end
+  end
+
+  puts
+  puts "parents:"
+  rails_list_tables(tables, :parents)
+
+  puts
+  puts "dependents:"
+  rails_list_tables(tables, :dependents)
+
+  puts
+  puts "associations:"
+  rails_list_tables(tables, :associations)
+
+  tables
 end
 
 def rails_attr_get_default(attributes, key)
@@ -302,29 +395,6 @@ def rails_modify_model(table_name, table_keys)
     f.puts renderer.result()
   end
 end
-
-#def rails_modify_helper(table_name, table_def)
-#  puts "=> Modify helper '" + keyword(table_name) + "'..."
-#
-#  @helper_name = keyword_plural(table_name)
-#  helper = "app/helpers/" + @helper_name + "_helper.rb"
-#  template = File.read("../helper.erb")
-#
-#  File.open(helper, "w") do |f|
-#    @class_name = keyword_camel(@helper_name)
-#    @attrs_def = table_def["attributes"]
-#
-#    @default_def = Hash.new
-#    if @attrs_def != nil
-#      @attrs_def.each do |k, v|
-#        @default_def[keyword(k)] = rails_attr_get_default(@attrs_def, k)
-#      end
-#    end
-#
-#    renderer = ERB.new(template, nil, '<>')
-#    f.puts renderer.result()
-#  end
-#end
 
 def rails_api_path(table_name, heritage)
   path = Array.new
@@ -486,31 +556,6 @@ def rails_keys_constraints(table_keys)
   cons
 end
 
-def rails_add_routes(table_name, table_keys, api_path)
-  puts "=> Add routes '" + keyword(table_name) + "' ..."
-
-  route = Hash.new
-  route[:name] = keyword_plural(table_name)
-  route[:path] = api_path
-  route[:cons] = rails_keys_constraints(table_keys)
-  $routes << route
-  $routes_resource << keyword_plural(table_name)
-end
-
-def rails_add_polymorphic_routes(table_name_assoc, index_keys,
-                                 table_name_polymorphic, api_path)
-  puts "=> Add routes '" + keyword(table_name_assoc) + "' ..."
-
-  cons = rails_keys_constraints(index_keys)
-  route = Hash.new
-  route[:name] = keyword(table_name_assoc)
-  route[:path] = api_path
-  route[:polymorphic] = keyword(table_name_polymorphic)
-  route[:cons] = cons
-
-  $routes_polymorphic << route
-end
-
 def rails_get_heritage(table_name)
   heritage = Array.new
 
@@ -569,6 +614,31 @@ def rails_get_db_fields(table_def, table_keys)
   db_fields
 end
 
+def rails_add_routes(table_name, table_keys, api_path)
+  puts "=> Add routes for '" + keyword(table_name) + "' ..."
+
+  route = Hash.new
+  route[:name] = keyword_plural(table_name)
+  route[:path] = api_path
+  route[:cons] = rails_keys_constraints(table_keys)
+  $routes << route
+  $routes_resource << keyword_plural(table_name)
+end
+
+def rails_add_polymorphic_routes(table_name_assoc, index_keys,
+                                 table_name_polymorphic, api_path)
+  puts "=> Add routes for '" + keyword(table_name_assoc) + "' ..."
+
+  cons = rails_keys_constraints(index_keys)
+  route = Hash.new
+  route[:name] = keyword(table_name_assoc)
+  route[:path] = api_path
+  route[:polymorphic] = keyword(table_name_polymorphic)
+  route[:cons] = cons
+
+  $routes_polymorphic << route
+end
+
 def rails_add_table(table_name)
   table_def = $table2json[table_name]
   if table_def.nil?
@@ -580,20 +650,16 @@ def rails_add_table(table_name)
     db_fields = rails_get_db_fields(table_def, table_keys)
     api_path = rails_api_path(table_name, heritage)
 
-    # We do scaffolding only if model doesn't exist.
-    # We should have better granularity.
-    model = "app/models/" + keyword(table_name) + ".rb"
-    if !File.exists?(model) and $options[:scaffold]
-      # Scaffolding
-      rails_cmd = "rails generate scaffold " +
-        keyword(table_name) + " " +
-        db_fields.map{|a| a[0] + ":" + a[1] }.join(" ")
-      puts "=> Scaffolding " + keyword(table_name) + " ..."
-      puts rails_cmd
-      system(rails_cmd)
+    # New table, scaffold.
+    if $tables[:flag][table_name] == "N"
+      rails_scaffold(table_name, db_fields, false)
+    # Updated table, downgrade first and then scaffold --force
+    elsif $tables[:flag][table_name] == "U"
+      rails_rake_db_migrate_down(table_name)
+      rails_scaffold(table_name, db_fields, true)
     end
 
-    if $options[:migration]
+    if $tables[:flag][table_name] == "N" or $tables[:flag][table_name] == "U"
       # Migration: generate index's
       rails_db_add_index(table_name, table_keys)
 
@@ -602,24 +668,22 @@ def rails_add_table(table_name)
     end
 
     # Model: add association
-    if $options[:model]
+    if $tables[:flag][table_name] == "N" or $tables[:flag][table_name] == "U"
       rails_modify_model(table_name, table_keys)
     end
 
     # Controller: add custom update/destroy methods
-    if $options[:controller]
+    if $tables[:flag][table_name] == "N" or $tables[:flag][table_name] == "U"
       rails_modify_controller(table_name, table_keys, db_fields, api_path)
     end
 
     # View: generate cli.erb
-    if $options[:view]
+    if $tables[:flag][table_name] == "N"
       rails_generate_view(table_name)
     end
 
     # Routes: add routes
-    if $options[:routes]
-      rails_add_routes(table_name, table_keys, api_path)
-    end
+    rails_add_routes(table_name, table_keys, api_path)
 
     # Iterate children recursively
     children = table_def["has-dependent"]
@@ -699,19 +763,16 @@ def rails_add_association(table_name)
     table_keys = rails_get_table_keys_assoc(table_def)
     db_fields = rails_get_db_fields(table_def, table_keys)
 
-    # We do scaffolding only if model doesn't exist.
-    # We should have better granularity.
-    model = "app/models/" + keyword(table_name) + ".rb"
-    if !File.exists?(model) and $options[:scaffold]
-      # Scaffolding
-      rails_cmd = "rails generate scaffold " +
-        keyword(table_name) + " " + db_fields.join(" ")
-      puts "=> Scaffolding '" + keyword(table_name) + "' ..."
-      puts rails_cmd
-      system(rails_cmd)
+    # New table, scaffold.
+    if $tables[:flag][table_name] == "N"
+      rails_scaffold(table_name, db_fields, false)
+    # Updated table, downgrade first and then scaffold --force
+    elsif $tables[:flag][table_name] == "U"
+      rails_rake_db_migrate_down(table_name)
+      rails_scaffold(table_name, db_fields, true)
     end
 
-    if $options[:migration]
+    if $tables[:flag][table_name] == "N" or $tables[:flag][table_name] == "U"
       # Migration: generate index's
       rails_db_add_index(table_name, table_keys)
 
@@ -720,86 +781,41 @@ def rails_add_association(table_name)
     end
 
     # Model: add association
-    if $options[:model]
+    if $tables[:flag][table_name] == "N" or $tables[:flag][table_name] == "U"
       rails_modify_model(table_name, table_keys)
     end
 
     # View: generate cli.erb
-    if $options[:view]
+    if $tables[:flag][table_name] == "N"
       rails_generate_view(table_name)
     end
 
     api_path = nil
 
     # Routes: add routes
-    if $options[:routes]
-      belongs_to, polymorphic = rails_get_belongs_to_assoc(table_def)
-      if !polymorphic.nil?
-        intf = polymorphic["interface"]
-        polymorphic["table"].each do |t|
-          api_path = rails_api_path_polymorphic(belongs_to, t)
-          index_keys = rails_get_index_keys_assoc(belongs_to, t)
-          rails_add_polymorphic_routes(table_name, index_keys, t, api_path)
-        end
-
-        api_path = nil
-      else
-        api_path = rails_api_path_polymorphic(belongs_to, nil)
-        index_keys = rails_get_index_keys_assoc(belongs_to, nil)
-        rails_add_routes(table_name, index_keys, api_path)
+    belongs_to, polymorphic = rails_get_belongs_to_assoc(table_def)
+    if !polymorphic.nil?
+      intf = polymorphic["interface"]
+      polymorphic["table"].each do |t|
+        api_path = rails_api_path_polymorphic(belongs_to, t)
+        index_keys = rails_get_index_keys_assoc(belongs_to, t)
+        rails_add_polymorphic_routes(table_name, index_keys, t, api_path)
       end
 
-      $routes_resource << keyword_plural(table_name)
+      api_path = nil
+    else
+      api_path = rails_api_path_polymorphic(belongs_to, nil)
+      index_keys = rails_get_index_keys_assoc(belongs_to, nil)
+      rails_add_routes(table_name, index_keys, api_path)
     end
 
+    $routes_resource << keyword_plural(table_name)
+
     # Controller: add custom update/destroy methods
-    if $options[:controller]
+    if $tables[:flag][table_name] == "N" or $tables[:flag][table_name] == "U"
       rails_modify_controller(table_name, nil, db_fields, api_path)
     end
   end
-end
-
-def rails_load_tables(dir)
-  parents = Array.new
-  associations = Array.new
-  table2json = Hash.new
-
-  m = Dir.entries(dir).select {|f| f =~ /\.table\.json$/}
-  m.each do |f|
-    file = dir + "/" + f
-
-    str = File.read(file)
-    json = JSON.parse(str)
-
-    if json["table"] != nil
-      json["table"].each do |t, obj|
-        if obj["type"] != nil
-          if obj["type"] == "parent"
-            parents << t
-          elsif obj["type"] == "association"
-            associations << t
-          end
-        end
-
-        table2json[t] = obj
-      end
-    end
-  end
-
-  puts "parents:"
-  parents.each do |p|
-    puts "- " + keyword(p)
-  end
-
-  puts ""
-  puts "associations:"
-  associations.each do |a|
-    puts "- " + keyword(a)
-  end
-
-  puts
-
-  [parents, associations, table2json]
 end
 
 def rails_update_mime_types
@@ -865,8 +881,8 @@ def main(dir)
   Dir.chdir($rails_project)
 
   # Load *.table.json to get list of parents, association and table2json.
-  parents, associations, table2json = rails_load_tables(table_json_dir)
-  $table2json = table2json
+  $tables = rails_load_tables(table_json_dir)
+  $table2json = $tables[:json]
 
   # Routes.
   $routes = Array.new
@@ -874,12 +890,12 @@ def main(dir)
   $routes_polymorphic = Array.new
 
   # Iterate from parents, and then iterate children recursively.
-  parents.each do |p|
+  $tables[:parents].each do |p|
     rails_add_table(p)
   end
 
   # Iterate from associations.
-  associations.each do |a|
+  $tables[:associations].each do |a|
     rails_add_association(a)
   end
 
@@ -887,44 +903,54 @@ def main(dir)
   rails_update_routes
 
   # Update mime.types
-  rails_update_mime_types if $options[:mime] == true
+  rails_update_mime_types
 
   # rake db:migrate
-  puts "=> Run DB migration ..."
-  system("rake db:migrate")
+  rails_rake_db_migrate
+end
+
+
+def show_help
+  puts "Usage: this-script <rails-project> <table-def-dir> [options...]"
+  exit
+end
+
+# Mandatory arguments
+#  [0] rails project
+#  [1] table.json directory
+#
+if ARGV[0] != nil
+  $rails_project = ARGV[0]
+else
+  $rails_project = "railsapp"
+end  
+
+if ARGV[1] != nil
+  db_dir = ARGV[1]
+else
+  db_dir = "../db"
 end
 
 #
 # Options
 #
 $options = Hash.new
-set_options_all($options)
 
 opts = GetoptLong.new(
-  [ '--help',            '-h', GetoptLong::NO_ARGUMENT ],
-  [ '--model-only',      '-m', GetoptLong::NO_ARGUMENT ],
-  [ '--controller-only', '-c', GetoptLong::NO_ARGUMENT ],
-  [ '--view-only',       '-v', GetoptLong::NO_ARGUMENT ]
+  [ '--help',            '-h', GetoptLong::NO_ARGUMENT ]
+#  [ '--model-only',      '-m', GetoptLong::NO_ARGUMENT ],
+#  [ '--controller-only', '-c', GetoptLong::NO_ARGUMENT ],
+#  [ '--view-only',       '-v', GetoptLong::NO_ARGUMENT ]
 )
 
 opts.each do |opt, arg|
   case opt
     when '--help'
       show_help
-    when '--model-only'
-      unset_options_all($options)
-      $options[:model] = true
-      $options[:scaffold] = false
-    when '--controller-only'
-      unset_options_all($options)
-      $options[:controller] = true
-      options[:scaffold] = true
-    when '--view-only'
-      unset_options_all($options)
-      $options[:view] = true
-      $options[:scaffold] = true
   end
 end
 
-# Start from here
+#
+# Start this script from here.
+#
 main(db_dir)
