a65f646193d5f53977a5fe6b074172b7de598a3f
Toshiaki Takada <toshiaki.takada@gmail.com>
2014-08-26 14:33:26 +0000

    Route-map initial commit, need more testing.

--- www/routes/routes_show.rb
+++ www/routes/routes_show.rb
@@ -1,3 +1,4 @@
   # Show controller.
   get "api/show/running-config(.:format)", to: "shows#running_config"
   get "api/show/access-list(.:format)", to: "shows#access_list"
+  get "api/show/interfaces(.:format)", to: "shows#interfaces"
